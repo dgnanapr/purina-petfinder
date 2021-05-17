089d71dfc851e0d3698404d1e86a476fb51a4ca0
dillibabuDeltax <deltaxworldIT@gmail.com>
2021-05-17 17:04:23 -0500

    commit

--- aurora-dynamo-sync/rds_cdc_TR_animals_AI.sql
+++ aurora-dynamo-sync/rds_cdc_TR_animals_AI.sql
@@ -1,5 +1,5 @@
--- Step 3 Create rds_cdc_TR_animals_AI  TRIGGER , this triggers calls the rds_lambda_cdc to send cdc data to a common lambda function rds_lamdbda_sns
--- DROP TRIGGER rds_cdc_TR_animals_AI;
+--  Step 3 Create rds_cdc_TR_animals_AI  TRIGGER , this triggers calls the rds_lambda_cdc to send cdc data to a common lambda function rds_lamdbda_sns
+--  DROP TRIGGER rds_cdc_TR_animals_AI;
 DELIMITER ;;
 CREATE TRIGGER rds_cdc_TR_animals_AI
   AFTER INSERT ON animals
@@ -9,9 +9,18 @@ BEGIN
   -- CALL rds_lambda_cdc('animals', @cdc_payload , @operation);
  -- CALL rds_lambda_cdc('animals', 'PAYLOAD' , 'INSERT');
 -- CALL rds_lambda_cdc('animals', JSON_OBJECT("id", NEW.id,"description", NEW.description), 'INSERT');
-SELECT CONCAT('{ \'id\' : \'', NEW.id, '\',  \'name\' : \'', NEW.description, '\'}'), 'i' INTO @id, @flag;
+-- SELECT CONCAT('{ \'id\' : \'', NEW.id, '\',  \'name\' : \'', NEW.description, '\'}'), 'i' INTO @id, @flag;
+ -- SELECT CONCAT('{ \'id\' : \'', NEW.id, '\', \'status\' : \'', NEW.status, '\',  \'created_by\' : \'', NEW.created_by, '\',  \'created_by\' : \'',NEW.created_by, '\',  \'description\' : \'',NEW.description,  '\'}'), 'i' INTO @id, @flag;
 -- SELECT New.id,  New.description INTO @id, @flag;
-CALL rds_lambda_cdc('animals', @id  , @flag);
+DECLARE payload TEXT;
+IF NEW.id is NOT NULL THEN
+SET payload = (SELECT CONCAT('{ \'id\' : \'', NEW.id, '\','));
+END IF;
+IF NEW.description is NOT NULL THEN
+SET payload = CONCAT(payload,'\'description\' : \'', NEW.description, '\',');
+END IF;
+SET payload=CONCAT(payload, '\'}');
+CALL rds_lambda_cdc('animals', 'i'  ,payload);
 END
 ;;
 DELIMITER ; 
--- aurora-dynamo-sync/rds_lambda_cdc.sql
+++ aurora-dynamo-sync/rds_lambda_cdc.sql
@@ -1,13 +1,13 @@
 -- Step 2 Create rds_lambda_sync  procedure , this the stored procedure call by all table triggers to send cdc data to a common lambda function rds_lamdbda_sns
 DROP PROCEDURE IF EXISTS rds_lambda_cdc;
 DELIMITER ;;
-CREATE DEFINER=`pfadmin`@`%` PROCEDURE `rds_lambda_cdc`(IN tableName VARCHAR(255),  IN cdc_payload TEXT, IN operation VARCHAR(255))
+CREATE DEFINER=`pfadmin`@`%` PROCEDURE `rds_lambda_cdc`(IN tableName VARCHAR(255), IN cdc_operation VARCHAR(255),  IN cdc_payload TEXT )
 BEGIN
     CALL mysql.lambda_async(
          'arn:aws:lambda:us-east-1:462477274376:function:RdsCdcReceiver',
          CONCAT('{"table_name" : "', tableName,
-             '", "pk" : "', cdc_payload,
-             '", "operation" : "', operation,'"}')
+             '", "cdc_operation" : "', cdc_operation,
+             '", "cdc_payload" : "', cdc_payload,'"}')
      );
   END
   ;;
