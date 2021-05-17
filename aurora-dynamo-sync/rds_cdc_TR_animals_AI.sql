47ac01fd83895f21a95f13c809c3d214ce484c7a
dillibabuDeltax <deltaxworldIT@gmail.com>
2021-05-17 17:54:53 -0500

    working with multiple null check

--- aurora-dynamo-sync/rds_cdc_TR_animals_AI.sql
+++ aurora-dynamo-sync/rds_cdc_TR_animals_AI.sql
@@ -13,13 +13,44 @@ BEGIN
  -- SELECT CONCAT('{ \'id\' : \'', NEW.id, '\', \'status\' : \'', NEW.status, '\',  \'created_by\' : \'', NEW.created_by, '\',  \'created_by\' : \'',NEW.created_by, '\',  \'description\' : \'',NEW.description,  '\'}'), 'i' INTO @id, @flag;
 -- SELECT New.id,  New.description INTO @id, @flag;
 DECLARE payload TEXT;
-IF NEW.id is NOT NULL THEN
 SET payload = (SELECT CONCAT('{ \'id\' : \'', NEW.id, '\','));
+IF NEW.status is NOT NULL THEN
+    SET payload = CONCAT(payload,'\'status\' : \'', NEW.status, '\',');
+END IF;
+IF NEW.created_by is NOT NULL THEN
+    SET payload = CONCAT(payload,'\'created_by\' : \'', NEW.created_by, '\','); 
+END IF;
+IF NEW.created_by_client_id is NOT NULL THEN
+    SET payload = CONCAT(payload,'\'created_by_client_id\' : \'', NEW.created_by_client_id, '\','); 
+END IF;
+IF NEW.created_at is NOT NULL THEN
+	SET payload = CONCAT(payload,'\'created_at\' : \'', NEW.created_at, '\',');
+END IF;
+IF NEW.updated_at is NOT NULL THEN
+	SET payload = CONCAT(payload,'\'updated_at\' : \'', NEW.updated_at, '\',');
+END IF;
+IF NEW.updated_by is NOT NULL THEN
+	SET payload = CONCAT(payload,'\'updated_by\' : \'', NEW.updated_by, '\',');
+END IF;
+IF NEW.updated_by_client_id is NOT NULL THEN
+	SET payload = CONCAT(payload,'\'updated_by_client_id\' : \'', NEW.updated_by_client_id, '\',');
+END IF;
+IF NEW.updated_by_user_id is NOT NULL THEN
+	SET payload = CONCAT(payload,'\'updated_by_user_id\' : \'', NEW.updated_by_user_id, '\',');
+END IF;
+IF NEW.arrival_date is NOT NULL THEN
+	SET payload = CONCAT(payload,'\'arrival_date\' : \'', NEW.arrival_date, '\',');
+END IF;
+IF NEW.adoption_date is NOT NULL THEN
+	SET payload = CONCAT(payload,'\'adoption_date\' : \'', NEW.adoption_date, '\',');
+END IF;
+IF NEW.birth_date is NOT NULL THEN
+	SET payload = CONCAT(payload,'\'birth_date\' : \'', NEW.birth_date, '\',');
 END IF;
 IF NEW.description is NOT NULL THEN
-SET payload = CONCAT(payload,'\'description\' : \'', NEW.description, '\',');
+	SET payload = CONCAT(payload,'\'description\' : \'', NEW.description, '\',');
 END IF;
-SET payload=CONCAT(payload, '\'}');
+	SET payload=CONCAT(payload, '\'}');
 CALL rds_lambda_cdc('animals', 'i'  ,payload);
 END
 ;;
