fffc713640eddaea2067b184a28fb3eb72e35454
dillibabuDeltax <deltaxworldIT@gmail.com>
2021-06-04 03:11:58 -0500

    included id of the record in lambda call

--- aurora-dynamo-sync/rds_cdc_TR_animals_AI.sql
+++ aurora-dynamo-sync/rds_cdc_TR_animals_AI.sql
@@ -6,7 +6,7 @@ CREATE TRIGGER rds_cdc_TR_animals_AI
 BEGIN
 DECLARE payload TEXT;
 CALL construct_animals_rds_cdc(NEW.id, payload);
-CALL rds_lambda_cdc('animals', 'i'  ,payload);
+CALL rds_lambda_cdc('animals', NEW.id,'i'  ,payload);
 END
 ;;
 DELIMITER ; 
